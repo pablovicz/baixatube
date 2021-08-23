868bd782782510fdf043f9e5f93bd88c6faea68b
Pablo Woinarovicz Ramos <pablowoina2205@gmail.com>
2021-08-23 18:54:29 -0300

    Prod Changes

--- baixatube-webapp/src/service/api.js
+++ baixatube-webapp/src/service/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 const api = axios.create({
-    baseURL: 'http://localhost:8000/'
+    baseURL: `${BASE_URL}/`
 });
 
 export default api;
